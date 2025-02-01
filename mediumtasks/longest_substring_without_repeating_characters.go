cebe7f0dada82a1b6226db0fd6e3cac6f8bb4403
lucas-10101 <lucas-quadros@outlook.com.br>
2025-02-01 18:07:46 -0300

    Runtime improvement for: 3. Longest Substring Without Repeating Characters
     - Improved execution time for lengthOfLongestSubstring method (withoud sliding technique)
     - 300 -> 150 ms (50% + performance and not optimal performance)
    
     - clear for map still has issues, but improvements come later

--- mediumtasks/longest_substring_without_repeating_characters.go
+++ mediumtasks/longest_substring_without_repeating_characters.go
@@ -1,32 +1,23 @@
 package mediumtasks
 
 func lengthOfLongestSubstring(s string) int {
+
 	max := 0
-	for i := 0; i < len(s); i++ {
-
-		j := i + 1
-		chars := map[byte]*int{s[i]: nil}
-
-		for ; j < len(s); j++ {
-			_, present := chars[s[j]]
-
-			if present {
-				if max < len(s[i:j]) {
-					max = len(s[i:j])
-				}
-				break
-			} else {
-				chars[s[j]] = nil
-			}
-		}
+	chars := map[byte]int{}
+	for i := 0; i < len(s); {
+		old, present := chars[s[i]]
+		chars[s[i]] = i
 
 		if max < len(chars) {
 			max = len(chars)
 		}
-	}
 
-	if max == 0 {
-		return len(s)
+		if present {
+			clear(chars)
+			i = old + 1
+			continue
+		}
+		i++
 	}
 
 	return max
